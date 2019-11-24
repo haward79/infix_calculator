ca5e88d483991ab1e64bf9f1bb8b3077bd1d7f9b
haward79 <haward79@yahoo.com.tw>
2019-11-24 15:49:02 +0800

    Remove try catch for out of index exception in main function.

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,12 @@ int main()
     InfixExpr& infix = *(new InfixExpr);
     PostfixExpr& postfix = *(new PostfixExpr);
 
-    try
-    {
-        // Read an infix expression (and split to tokens).
-        infix.input();
+    // Read an infix expression (and split to tokens).
+    infix.input();
 
-        // Infix to postfix.
-        postfix.setExpr(infix.toPostfixTokens());
-        cout << "Answer: " << postfix.evaluate() << "\n";
-    }
-    catch(OutOfIndexException e)
-    {
-        cout << "\n" << e.toString() << "\n\n";
-    }
+    // Infix to postfix.
+    postfix.setExpr(infix.toPostfixTokens());
+    cout << "Answer: " << postfix.evaluate() << "\n";
 
     // Clear heap.
     delete &infix;
