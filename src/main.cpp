6d5ae9b5dd35d16d5796a6256cfd1ae1fe08f6dc
haward79 <haward79@yahoo.com.tw>
2019-11-26 09:09:07 +0800

    Add general try catch to main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "InfixExpr.h"
 #include "PostfixExpr.h"
-#include "OutOfIndexException.h"
+#include "Exception.h"
 
 using NutnDS::InfixExpr;
 using NutnDS::PostfixExpr;
@@ -11,12 +11,19 @@ int main()
     InfixExpr& infix = *(new InfixExpr);
     PostfixExpr& postfix = *(new PostfixExpr);
 
-    // Read an infix expression (and split to tokens).
-    infix.input();
+    try
+    {
+        // Read an infix expression (and split to tokens).
+        infix.input();
 
-    // Infix to postfix.
-    postfix.setExpr(infix.toPostfixTokens());
-    cout << "Answer: " << postfix.evaluate() << "\n";
+        // Infix to postfix.
+        postfix.setExpr(infix.toPostfixTokens());
+        cout << "Answer: " << postfix.evaluate() << "\n";
+    }
+    catch(Exception e)
+    {
+        cout << e.toString() << "\n";
+    }
 
     // Clear heap.
     delete &infix;
